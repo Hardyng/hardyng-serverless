1e3eef59e6f610c5d9caec65fb0225d1da3c0f23
Adam Kalinowski <adam.kalinowski.1997@gmail.com>
2019-01-13 17:10:50 +0100

    With webpack

--- services/subscriptions/handler.js
+++ services/subscriptions/handler.js
@@ -1,18 +1,15 @@
 const uuid = require('uuid');
-const dynamoDbLib = require('../../libs/dynamodb-lib');
-const { success, failure } = require('../../libs/response-lib');
+const dynamoDbLib = require('../../lib/dynamodb-lib');
+const { success, failure } = require('../../lib/response-lib');
 
-module.exports.main = async (event, context, callback) => {
-  // const data = JSON.parse({
-  //   name: '123',
-  // });
+export const main = async (event, context, callback) => {
+  const data = JSON.parse(event.body);
   const params = {
     TableName: "dev-mono-notes",
     Item: {
-      // userId: event.requestContext.identity.cognitoIdentityId,
-      userId: '999',
+      userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
-      name: '123',
+      name: data.name,
       createdAt: Date.now()
     }
   };
@@ -21,41 +18,6 @@ module.exports.main = async (event, context, callback) => {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log(e);
     return failure({ status: false });
   }
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
-  //
-  // callback(null, response);
 };
-
-// import uuid from "uuid";
-// import * as dynamoDbLib from "../../libs/dynamodb-lib";
-// import { success, failure } from "../../libs/response-lib";
-//
-// export async function main(event, context) {
-//   // const data = JSON.parse(event.body);
-//   console.log('test');
-//   const params = {
-//     TableName: "notes",
-//     Item: {
-//       userId: event.requestContext.identity.cognitoIdentityId,
-//       noteId: uuid.v1(),
-//       title: 'test',
-//       createdAt: Date.now()
-//     }
-//   };
-//
-//   try {
-//     await dynamoDbLib.call("put", params);
-//     return success(params.Item);
-//   } catch (e) {
-//     return failure({ status: false });
-//   }
-// }
